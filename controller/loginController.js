79c976ef9911dfe314e6c51c9d8ab73eefa5f209
githubcyj <2503975458@qq.com>
2019-12-02 17:04:22 +0800

    20191202

--- controller/loginController.js
+++ controller/loginController.js
@@ -4,7 +4,7 @@ const LoginService = require('../service/loginService')
 var getUserInfo = async(ctx) => {
     const {username, password} = ctx.request.body
     let loginData = await LoginService.login(username, password)
-    ctx.body = loginData.data
+    ctx.body = loginData
 }
 
 module.exports = {getUserInfo: getUserInfo}
