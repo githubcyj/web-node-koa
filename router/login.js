77fc3de84f635432cc0cfad017cfaf764cec6a97
yanjiao.cao <yanjiao.cao@weimob.com>
2020-06-08 10:26:14 +0800

    react/lkmanager_api

--- router/login.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-
-const LoginController = require('../controller/loginController')
-
-const router = new Router()
-
-router.post('/', LoginController.getUserInfo)
-
-module.exports = router.routes()
